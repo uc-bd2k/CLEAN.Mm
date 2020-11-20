cc96fe9fd1c8206a9793f7560a86e5bf5e28f4dc
Mario-Medvedovic <Mario.Medvedovic@uc.edu>
2020-11-20 11:54:06 -0500

    CMcells

--- R/CLEAN.Mm.R
+++ R/CLEAN.Mm.R
@@ -1,4 +1,4 @@
 CLEAN.Mm <-
 function() c("chrLoc", "geneRIFs", "GO", "GSEA", "HUGE", "KEGG", "L2L", 
-"MeshTable", "modules", "MP", "reactome", "transfac","CTD")
+"MeshTable", "modules", "MP", "reactome", "transfac","CTD","dorothea","tfacts","CMcells")
 
