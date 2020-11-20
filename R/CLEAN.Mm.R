579bc34a31e6e2476bf547e63f6776ab47a9ebe4
Mario-Medvedovic <Mario.Medvedovic@uc.edu>
2020-11-20 13:48:14 -0500

    hallmark

--- R/CLEAN.Mm.R
+++ R/CLEAN.Mm.R
@@ -1,4 +1,4 @@
 CLEAN.Mm <-
 function() c("chrLoc", "geneRIFs", "GO", "GSEA", "HUGE", "KEGG", "L2L", 
-"MeshTable", "modules", "MP", "reactome", "transfac","CTD","dorothea","tfacts","CMcells","HCLcells","HPAcells","tissues")
+"MeshTable", "modules", "MP", "reactome", "transfac","CTD","dorothea","tfacts","CMcells","HCLcells","HPAcells","tissues","hallmark")
 
