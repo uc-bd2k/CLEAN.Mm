446edd1b214ff95849e5cee58556b46027ab23f5
Mario-Medvedovic <Mario.Medvedovic@uc.edu>
2022-10-18 16:57:24 -0400

    adding .gitignore

--- R/CLEAN.Mm.R
+++ R/CLEAN.Mm.R
@@ -1,4 +1,4 @@
 CLEAN.Mm <-
 function() c("chrLoc", "geneRIFs", "GO", "GSEA", "HUGE", "KEGG", "L2L", 
-"MeshTable", "modules", "MP", "reactome", "transfac","CTD","dorotheatfs","tfacts","CMcells","HCLcells","HPAcells","tissues","hallmark")
+"MeshTable", "modules", "MP", "reactome", "transfac","CTD","dorothea","tfacts","CMcells","HCLcells","HPAcells","tissues","hallmark")
 
